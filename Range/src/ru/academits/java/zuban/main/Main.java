f41d089aebeb56136fb519ba574174c1243a3bd7
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-07-13 21:10:10 +0700

    Доработка задачи Range

---
+++ Range/src/ru/academits/java/zuban/main/Main.java
@@ -0,0 +1,32 @@
+package ru.academits.java.zuban.main;
+
+import ru.academits.java.zuban.range.Range;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Range range1 = new Range(3, 9);
+        Range range2 = new Range(5, 12);
+
+        Range result = range1.findIntersection(range2);
+        System.out.println("intersections " + result);
+
+        Range[] union = range1.createUnion(range2);
+        System.out.println("union " + Arrays.toString(union));
+
+        Range[] difference = range1.calculateDifference(range2);
+        System.out.println("difference " + Arrays.toString(difference));
+
+        range1 = new Range(-2, 3);
+        range2 = new Range(4, 10);
+
+        result = range1.findIntersection(range2);
+        System.out.println("intersections " + result);
+
+        union = range1.createUnion(range2);
+        System.out.println("union " + Arrays.toString(union));
+
+        difference = range1.calculateDifference(range2);
+        System.out.println("difference " + Arrays.toString(difference));
+    }
+}
---
+++ Range/src/ru/academits/java/zuban/range/Range.java
@@ -0,0 +1,83 @@
+package ru.academits.java.zuban.range;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + from + ", " + to + '}';
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        return number <= to && number >= from;
+    }
+
+    public Range findIntersection(Range range) {
+        if (range.to < from || range.from > to) {
+            return null;
+        }
+
+        double start = Math.min(from, range.from);
+        double end = Math.max(to, range.to);
+
+        return new Range(start, end);
+    }
+
+    public Range[] createUnion(Range range) {
+        if (range.to < from || range.from > to) {
+            return new Range[]{new Range(from, to)};
+        }
+
+        double start = Math.min(from, range.from);
+        double end = Math.max(to, range.to);
+
+        return new Range[]{new Range(start, end)};
+    }
+
+    public Range[] calculateDifference(Range range) {
+        if (range.from > to || range.to < from) {
+            return null;
+        } else if (range.from >= from && range.to <= to) {
+            return new Range[]{new Range(from, range.from), new Range(range.to, to)};
+        } else if (range.from <= from && range.to >= to) {
+            return new Range[]{new Range(range.from, from), new Range(to, range.to)};
+        } else if (range.from < from && range.to > from) {
+            return new Range[]{new Range(from, range.to)};
+        } else if (range.from > from && range.from < to) {
+            return new Range[]{new Range(range.from, to)};
+        }
+
+        return null;
+    }
+}
