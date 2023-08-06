f6fec55f9922732fa383f0be9c4c2ecafb6348cd
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-08-06 15:15:05 +0700

    задача решена '+'

--- Range/src/ru/academits/java/zuban/range_main/Main.java
+++ Range/src/ru/academits/java/zuban/range_main/Main.java
@@ -8,35 +8,35 @@ public class Main {
         Range range1 = new Range(3, 9);
         Range range2 = new Range(5, 1);
 
-        printOperations(range1, range2);
+        printRangeResults(range1, range2);
 
         range1 = new Range(-2, 3);
         range2 = new Range(4, 10);
 
-        printOperations(range1, range2);
+        printRangeResults(range1, range2);
 
         range1 = new Range(1, 5);
         range2 = new Range(3, 7);
 
-        printOperations(range1, range2);
+        printRangeResults(range1, range2);
 
         range1 = new Range(3, 7);
         range2 = new Range(1, 5);
 
-        printOperations(range1, range2);
+        printRangeResults(range1, range2);
 
         range1 = new Range(1, 5);
         range2 = new Range(1, 3);
 
-        printOperations(range1, range2);
+        printRangeResults(range1, range2);
 
         range1 = new Range(3, 7);
         range2 = new Range(5, 7);
 
-        printOperations(range1, range2);
+        printRangeResults(range1, range2);
     }
 
-    public static void printOperations(Range range1, Range range2) {
+    public static void printRangeResults(Range range1, Range range2) {
         System.out.println("First range: " + range1);
         System.out.println("Second range: " + range2);
 
