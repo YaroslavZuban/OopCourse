2c8c81aa894aceed29e36375279a65000dee13a1
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-08-17 15:44:23 +0700

    попровки

--- Tree/src/ru/academits/java/zuban/tree/Node.java
+++ Tree/src/ru/academits/java/zuban/tree/Node.java
@@ -1,9 +1,13 @@
 package ru.academits.java.zuban.tree;
 
-public class Node<E> {
+public class Node<E>{
     private E value;
     private Node<E> left;
     private Node<E> right;
+
+    public Node(E value) {
+        this.value = value;
+    }
 
     public Node(E value, Node<E> left, Node<E> right) {
         this.value = value;
--- Tree/src/ru/academits/java/zuban/tree/Tree.java
+++ Tree/src/ru/academits/java/zuban/tree/Tree.java
@@ -1,5 +1,14 @@
 package ru.academits.java.zuban.tree;
 
-public class Tree {
+public class Tree<T> {
+    private Node<T> peak;
+    private int size;
 
+    public void add(T value) {
+        if(peak==null){
+            peak=new Node<>(value);
+        }else{
+
+        }
+    }
 }
