c209ff4d0602fd71dd0ea17dccc71c622880a0b0
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-09-07 07:55:45 +0700

    CSV: остался реализовать пункт 18,21

--- CSV/src/ru/academits/java/zuban/csv_main/Main.java
+++ CSV/src/ru/academits/java/zuban/csv_main/Main.java
@@ -1,13 +1,21 @@
 package ru.academits.java.zuban.csv_main;
 
-import ru.academits.java.zuban.csv.CSV;
+import ru.academits.java.zuban.csv.Csv;
+
+import javax.imageio.IIOException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         String csvPath = "C:/Users/ben70/IdeaProjects/OopCourse/CSV/text.csv";
         String htmlPath = "html.html";
 
-        CSV cvs = new CSV();
-        cvs.conversionFileCvsToHtml(csvPath, htmlPath);
+        try {
+            new Csv().convertFileCsvToHtml(args[0], args[1]);
+        }catch (IOException e) {
+            System.out.println("Файл не получилось открыть.");
+        }
+
     }
 }
