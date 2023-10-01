b2c56247e2d7e077d4b77195719dce591a32670f
YaroslavZuban <72079338+YaroslavZuban@users.noreply.github.com>
2023-10-01 23:32:34 +0700

    Grapр: исправление 4

--- Graph/src/ru/academits/java/zuban/graph/Graph.java
+++ Graph/src/ru/academits/java/zuban/graph/Graph.java
@@ -26,25 +26,51 @@ public class Graph {
         }
 
         boolean[] visited = new boolean[adjacencyMatrix.length];
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            queue.add(i);
+            visited[i] = true;
+
+            while (!queue.isEmpty()) {
+                int node = queue.poll();
+                consumer.accept(node);
+
+                for (int j = 0; j < adjacencyMatrix.length; j++) {
+                    if (adjacencyMatrix[node][j] == 1 && !visited[j]) {
+                        queue.add(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public void traverseInDepthRecursive(IntConsumer consumer) {
+        if (adjacencyMatrix.length == 0) {
+            return;
+        }
+
+        boolean[] visited = new boolean[adjacencyMatrix.length];
 
         for (int i = 0; i < adjacencyMatrix.length; i++) {
             if (!visited[i]) {
-                Queue<Integer> queue = new ArrayDeque<>();
+                traverseInDepthRecursive(i, visited, consumer);
+            }
+        }
+    }
 
-                queue.add(i);
-                visited[i] = true;
+    private void traverseInDepthRecursive(int node, boolean[] visited, IntConsumer consumer) {
+        visited[node] = true;
+        consumer.accept(node);
 
-                while (!queue.isEmpty()) {
-                    int node = queue.poll();
-                    consumer.accept(node);
-
-                    for (int j = 0; j < adjacencyMatrix.length; j++) {
-                        if (adjacencyMatrix[node][j] == 1 && !visited[j]) {
-                            queue.add(j);
-                            visited[j] = true;
-                        }
-                    }
-                }
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[node][i] == 1 && !visited[i]) {
+                traverseInDepthRecursive(i, visited, consumer);
             }
         }
     }
@@ -54,49 +80,26 @@ public class Graph {
             return;
         }
 
+        Deque<Integer> stack = new ArrayDeque<>();
         boolean[] visited = new boolean[adjacencyMatrix.length];
 
         for (int i = 0; i < adjacencyMatrix.length; i++) {
-            if (!visited[i]) {
-                traverseInDepth(i, visited, consumer);
+            if (visited[i]) {
+                continue;
             }
-        }
-    }
 
-    private void traverseInDepth(int node, boolean[] visited, IntConsumer consumer) {
-        visited[node] = true;
-        consumer.accept(node);
+            stack.push(i);
 
-        for (int i = 0; i < adjacencyMatrix.length; i++) {
-            if (adjacencyMatrix[node][i] == 1 && !visited[i]) {
-                traverseInDepth(i, visited, consumer);
-            }
-        }
-    }
+            while (!stack.isEmpty()) {
+                int node = stack.pop();
 
-    public void bypassInDepth(IntConsumer consumer) {
-        if (adjacencyMatrix.length == 0) {
-            return;
-        }
+                if (!visited[node]) {
+                    consumer.accept(node);
+                    visited[node] = true;
 
-        boolean[] visited = new boolean[adjacencyMatrix.length];
-
-        for (int i = 0; i < adjacencyMatrix.length; i++) {
-            if (!visited[i]) {
-                Deque<Integer> stack = new ArrayDeque<>();
-                stack.push(i);
-
-                while (!stack.isEmpty()) {
-                    int node = stack.pop();
-
-                    if (!visited[node]) {
-                        consumer.accept(node);
-                        visited[node] = true;
-
-                        for (int j = adjacencyMatrix.length - 1; j >= 0; j--) {
-                            if (adjacencyMatrix[node][j] == 1 && !visited[j]) {
-                                stack.push(j);
-                            }
+                    for (int j = adjacencyMatrix.length - 1; j >= 0; j--) {
+                        if (adjacencyMatrix[node][j] == 1 && !visited[j]) {
+                            stack.push(j);
                         }
                     }
                 }
--- Graph/src/ru/academits/java/zuban/graph/test/GraphTest.java
+++ Graph/src/ru/academits/java/zuban/graph/test/GraphTest.java
@@ -215,205 +215,6 @@ class GraphTest {
     }
 
     @Test
-    protected void bypassInDepth() {
-        List<Integer> output1 = new ArrayList<>();
-        List<Integer> output2 = new ArrayList<>();
-
-        IntConsumer consumer1 = output1::add;
-
-        Graph graph1 = new Graph(new int[][]{
-                {0, 1, 1, 0, 0, 0, 1},
-                {1, 0, 1, 1, 0, 0, 0},
-                {1, 1, 0, 0, 0, 0, 0},
-                {0, 1, 0, 0, 1, 0, 0},
-                {0, 0, 0, 1, 0, 1, 0},
-                {0, 0, 0, 0, 1, 0, 1},
-                {1, 0, 0, 0, 0, 1, 0}
-        });
-
-        graph1.bypassInDepth(consumer1);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-        output2.add(3);
-        output2.add(4);
-        output2.add(5);
-        output2.add(6);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer2 = output1::add;
-
-        Graph graph2 = new Graph(new int[][]{
-                {0, 1, 1},
-                {1, 0, 1},
-                {1, 1, 0}
-        });
-
-        graph2.bypassInDepth(consumer2);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer3 = output1::add;
-
-        Graph graph3 = new Graph(new int[][]{
-                {0, 0, 0},
-                {0, 0, 0},
-                {0, 0, 0}
-        });
-
-        graph3.bypassInDepth(consumer3);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer4 = output1::add;
-
-        Graph graph4 = new Graph(new int[][]{
-                {0, 1, 0},
-                {1, 0, 1},
-                {0, 1, 0}
-        });
-
-        graph4.bypassInDepth(consumer4);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer5 = output1::add;
-
-        Graph graph5 = new Graph(new int[][]{
-                {0, 1, 1},
-                {1, 0, 0},
-                {1, 0, 0}
-        });
-
-        graph5.bypassInDepth(consumer5);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer6 = output1::add;
-
-        Graph graph6 = new Graph(new int[][]{
-                {0, 1, 1, 1},
-                {1, 0, 1, 0},
-                {1, 1, 0, 0},
-                {1, 0, 0, 0}
-        });
-
-        graph6.bypassInDepth(consumer6);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-        output2.add(3);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer7 = output1::add;
-
-        Graph graph7 = new Graph(new int[][]{
-                {0, 1, 0, 0, 1, 0},
-                {1, 0, 1, 0, 1, 0},
-                {0, 1, 0, 1, 0, 0},
-                {0, 0, 1, 0, 1, 1},
-                {1, 1, 0, 1, 0, 0},
-                {0, 0, 0, 1, 0, 0}
-        });
-
-        graph7.bypassInDepth(consumer7);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-        output2.add(3);
-        output2.add(4);
-        output2.add(5);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer8 = output1::add;
-
-        Graph graph8 = new Graph(new int[][]{
-                {0, 1, 1, 0, 0},
-                {1, 0, 0, 1, 0},
-                {1, 0, 0, 1, 0},
-                {0, 1, 1, 0, 1},
-                {0, 0, 0, 1, 0}
-        });
-
-        graph8.bypassInDepth(consumer8);
-        output2.add(0);
-        output2.add(1);
-        output2.add(3);
-        output2.add(2);
-        output2.add(4);
-
-        Assertions.assertEquals(output1, output2);
-
-        output1.clear();
-        output2.clear();
-
-        IntConsumer consumer9 = output1::add;
-
-        Graph graph9 = new Graph(new int[][]{
-                {0, 1, 0, 1, 0, 0},
-                {1, 0, 1, 0, 1, 0},
-                {0, 1, 0, 0, 0, 0},
-                {1, 0, 0, 0, 1, 1},
-                {0, 1, 0, 1, 0, 0},
-                {0, 0, 0, 1, 0, 0}
-        });
-
-        graph9.bypassInDepth(consumer9);
-
-        output2.add(0);
-        output2.add(1);
-        output2.add(2);
-        output2.add(4);
-        output2.add(3);
-        output2.add(5);
-
-        Assertions.assertEquals(output1, output2);
-    }
-
-    @Test
     protected void traverseInDepth() {
         List<Integer> output1 = new ArrayList<>();
         List<Integer> output2 = new ArrayList<>();
@@ -601,6 +402,205 @@ class GraphTest {
         });
 
         graph9.traverseInDepth(consumer9);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+        output2.add(4);
+        output2.add(3);
+        output2.add(5);
+
+        Assertions.assertEquals(output1, output2);
+    }
+
+    @Test
+    protected void traverseInDepthRecursive() {
+        List<Integer> output1 = new ArrayList<>();
+        List<Integer> output2 = new ArrayList<>();
+
+        IntConsumer consumer1 = output1::add;
+
+        Graph graph1 = new Graph(new int[][]{
+                {0, 1, 1, 0, 0, 0, 1},
+                {1, 0, 1, 1, 0, 0, 0},
+                {1, 1, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 1},
+                {1, 0, 0, 0, 0, 1, 0}
+        });
+
+        graph1.traverseInDepthRecursive(consumer1);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+        output2.add(3);
+        output2.add(4);
+        output2.add(5);
+        output2.add(6);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer2 = output1::add;
+
+        Graph graph2 = new Graph(new int[][]{
+                {0, 1, 1},
+                {1, 0, 1},
+                {1, 1, 0}
+        });
+
+        graph2.traverseInDepthRecursive(consumer2);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer3 = output1::add;
+
+        Graph graph3 = new Graph(new int[][]{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        });
+
+        graph3.traverseInDepthRecursive(consumer3);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer4 = output1::add;
+
+        Graph graph4 = new Graph(new int[][]{
+                {0, 1, 0},
+                {1, 0, 1},
+                {0, 1, 0}
+        });
+
+        graph4.traverseInDepthRecursive(consumer4);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer5 = output1::add;
+
+        Graph graph5 = new Graph(new int[][]{
+                {0, 1, 1},
+                {1, 0, 0},
+                {1, 0, 0}
+        });
+
+        graph5.traverseInDepthRecursive(consumer5);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer6 = output1::add;
+
+        Graph graph6 = new Graph(new int[][]{
+                {0, 1, 1, 1},
+                {1, 0, 1, 0},
+                {1, 1, 0, 0},
+                {1, 0, 0, 0}
+        });
+
+        graph6.traverseInDepthRecursive(consumer6);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+        output2.add(3);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer7 = output1::add;
+
+        Graph graph7 = new Graph(new int[][]{
+                {0, 1, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 0},
+                {0, 0, 1, 0, 1, 1},
+                {1, 1, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0}
+        });
+
+        graph7.traverseInDepthRecursive(consumer7);
+
+        output2.add(0);
+        output2.add(1);
+        output2.add(2);
+        output2.add(3);
+        output2.add(4);
+        output2.add(5);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer8 = output1::add;
+
+        Graph graph8 = new Graph(new int[][]{
+                {0, 1, 1, 0, 0},
+                {1, 0, 0, 1, 0},
+                {1, 0, 0, 1, 0},
+                {0, 1, 1, 0, 1},
+                {0, 0, 0, 1, 0}
+        });
+
+        graph8.traverseInDepthRecursive(consumer8);
+        output2.add(0);
+        output2.add(1);
+        output2.add(3);
+        output2.add(2);
+        output2.add(4);
+
+        Assertions.assertEquals(output1, output2);
+
+        output1.clear();
+        output2.clear();
+
+        IntConsumer consumer9 = output1::add;
+
+        Graph graph9 = new Graph(new int[][]{
+                {0, 1, 0, 1, 0, 0},
+                {1, 0, 1, 0, 1, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1, 1},
+                {0, 1, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 0}
+        });
+
+        graph9.traverseInDepthRecursive(consumer9);
 
         output2.add(0);
         output2.add(1);
